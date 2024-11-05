f8501dc1ca63525dae03f1dfc2fe101d438fdbb5
Iamalsauser <parthsinhv4@gmail.com>
2024-11-05 16:51:37 +0530

    Update: Personal_Template

---
+++ Athlinix/Preview Content/Score_integrate/template_personal.swift
@@ -0,0 +1,18 @@
+//
+//  template_personal.swift
+//  Athlinix
+//
+//  Created by admin17 on 05/11/24.
+//
+
+import SwiftUI
+
+struct template_personal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    template_personal()
+}
