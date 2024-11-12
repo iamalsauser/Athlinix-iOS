93b274589423b8e68c53d302da87b4c0eab856e6
Iamalsauser <parthsinhv4@gmail.com>
2024-11-12 13:57:45 +0530

    Update: Athlete_Position

--- Athlinix/Preview Content/starting/athlete_position.swift
+++ Athlinix/Preview Content/starting/athlete_position.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
-struct AthleteWeightScreen: View {
-    @State private var weight: Double = 70 // Default weight value
-    let weights: [Double] = Array(30...200).map { Double($0) } // Range of weights from 30 to 200 kg
+struct Athlete_position: View {
+    @State private var selectedSkillLevel: String = ""
     @Binding var selectedButton: String // Declare a binding for selectedButton
+    let skillLevels = ["Point Guard (PG)", "Shooting Guard (SG)", "Small Forward (SF)","Power Forward (PF)","Center (C)"]
 
     var body: some View {
         VStack {
-            Text("Enter your weight (kg)")
+            Text("What is your Position?")
                 .font(.title)
                 .padding()
 
-            // Customized Picker for weight with wheel style
-            Picker("Weight", selection: $weight) {
-                ForEach(weights, id: \.self) { weight in
-                    Text("\(Int(weight)) kg").tag(weight)
+            // Customized Picker for skill level with wheel style
+            Picker("Skill Level", selection: $selectedSkillLevel) {
+                ForEach(skillLevels, id: \.self) { level in
+                    Text(level).tag(level)
                 }
             }
             .pickerStyle(WheelPickerStyle()) // Use wheel style for a scrolling effect
             .frame(maxHeight: 200) // Adjust the height to suit the wheel display
             .padding()
 
-            NavigationLink(destination: Athlete_position(selectedButton: $selectedButton)) { // Pass the binding here
+            NavigationLink(destination: SkillLevelScreen(selectedButton: $selectedButton)) { // Pass the binding here
                 Text("Next")
                     .font(.headline)
                     .padding()
@@ -34,15 +34,15 @@ struct AthleteWeightScreen: View {
 
             Spacer()
         }
-        .navigationTitle("Weight")
+        .navigationTitle("Skill Level")
         .padding(.horizontal) // Add horizontal padding to the entire screen
     }
 }
 
-struct Weight_Previews: PreviewProvider {
+struct Position_previews: PreviewProvider {
     static var previews: some View {
         // Create a preview with a dummy binding
         @State var dummyButton: String = "home"
-        return AthleteWeightScreen(selectedButton: $dummyButton) // Pass a binding for preview
+        return Athlete_position(selectedButton: $dummyButton) // Pass a binding for preview
     }
 }
