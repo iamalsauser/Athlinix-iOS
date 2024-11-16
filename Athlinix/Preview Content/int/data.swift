8e5186dec68fd0dcc8eb151c7d5027fd98120fbe
Iamalsauser <parthsinhv4@gmail.com>
2024-11-16 23:40:19 +0530

    Update: Match history

---
+++ Athlinix/Preview Content/int/data.swift
@@ -0,0 +1,43 @@
+//
+//  data.swift
+//  MatchHistory
+//
+//  Created by admin65 on 14/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct Matchkit {
+    let homeTeamLogo: UIImage
+    let awayTeamLogo: UIImage
+    let homeTeamName: String
+    let awayTeamName: String
+    let fieldGoals: (home: Int, away: Int)
+    let threePointers: (home: Int, away: Int)
+    let freeThrows: (home: Int, away: Int)
+    let date: Date
+}
+
+//let matches: [Match] = [
+//    Match(
+//        homeTeamLogo: UIImage(named: "76ersLogo")!,
+//        awayTeamLogo: UIImage(named: "RaptorsLogo")!,
+//        homeTeamName: "76ers",
+//        awayTeamName: "Raptors",
+//        fieldGoals: (34, 24),
+//        threePointers: (10, 7),
+//        freeThrows: (6, 3),
+//        date: Date()
+//    ),
+//    Match(
+//        homeTeamLogo: UIImage(named: "LakersLogo")!,
+//        awayTeamLogo: UIImage(named: "KnicksLogo")!,
+//        homeTeamName: "Lakers",
+//        awayTeamName: "Knicks",
+//        fieldGoals: (47, 47),
+//        threePointers: (10, 7),
+//        freeThrows: (6, 3),
+//        date: Date()
+//    )
+//]
